ce5059366d8a241834b061c8b80614e877362cf7
Emre-11 <144242206+Emre-11@users.noreply.github.com>
2023-11-07 18:39:32 -0500

    Update handler.php

--- handler.php
+++ handler.php
@@ -9,14 +9,64 @@
 
   <body>
     
-    <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
+<?php
+
+$Large = 6;
+$ELarge = 10;
+$OneTopping = 1;
+$TwoTopping = 1.75;
+$ThreeTopping = 2.5 ;
+$FourTopping = 3.35;
+$Tax = 0.13;
+$askcondition = true;
+$askcondition2 = true;
+while ($askcondition) {
+    echo "<p>What size would you like? " ;
+    $PizzaS = readline("Large or Extra Large? ");
+
+    if ($PizzaS == "Large" || $PizzaS ==  "large" || $PizzaS == "Extra  Large" || $PizzaS == "extra large" || $PizzaS == "Extra large" || $PizzaS == "extra Large"){
+          $askcondition = false;
     }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
-    ?>
+}
+
+if ($PizzaS == "Large" || $PizzaS ==  "large") {
+    $PizzaCost = $Large;
+}
+if ($PizzaS == "Extra Large" || $PizzaS == "extra large" || $PizzaS == "Extra large" || $PizzaS == "extra Large") {
+    $PizzaCost = $ELarge;
+}
+
+while ($askcondition2) {
+    echo "How many toppings would you like? ";
+    $PizzaT = readline("1, 2, 3 or 4 ");
+    if ($PizzaT == "1" || "2" || "3" || "4") {
+        $askcondition2 = false;
+    }
+}
+
+if ($PizzaT == "1") {
+    $PizzaCost = $PizzaCost + $OneTopping;
+}
+if ($PizzaT == "2") {
+    $PizzaCost = $PizzaCost + $TwoTopping;
+}
+if ($PizzaT == "3") {
+    $PizzaCost = $PizzaCost + $ThreeTopping;
+}
+if ($PizzaT == "4") {
+    $PizzaCost = $PizzaCost + $FourTopping;
+}
+
+$Subtotal = $PizzaCost;
+echo " Subtotal: $". number_format($PizzaCost). PHP_EOL;
+$Tax = $Tax * $Subtotal;
+echo " Tax: $" . number_format($Tax, 2) . PHP_EOL;
+$Total = $Tax + $Subtotal;
+echo " Total: $" . number_format($Total, 2). PHP_EOL;
+
+
+
+?>
     
   </body>
   
